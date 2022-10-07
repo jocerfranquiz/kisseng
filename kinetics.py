1b801e7365d78bf4924062be9bb70a93e2e76a6d
newpaxonian <newpaxonian@gmail.com>
2022-10-06 21:17:01 -0500

    adding PGA3D for implementations using GA instead of Analytic Geometry

---
+++ kinetics.py
@@ -0,0 +1,15 @@
+"""
+Module with all the kinetics stuff
+20221004
+@Author: Pax
+"""
+
+
+class body:
+    def __init__(self, name, mass, pos, vel, acc):
+        self.name = name
+        self.mass = mass
+        self.pos = pos
+        self.vel = vel
+        self.acc = acc
+
